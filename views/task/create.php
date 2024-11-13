9dd6dca12a65e357e4c51124fbf298f2c5581a69
Enmanuel <end199615@gmail.com>
2024-11-13 18:20:38 -0500

    Contenedores con espaciado para los inputs y label del form  create.php

--- views/task/create.php
+++ views/task/create.php
@@ -9,20 +9,29 @@ require_once("C://xampp/htdocs/ProyectoTareas/views/head/head.php");
         <div class="mb-3">
             <label class="form-label">Título de la tarea</label>
             <input type="text" class="form-control" name="titulo" id="titulo" required>
+        </div>
+        <div class="mb-3">
             <label class="form-label">Descripción</label>
             <input type="text" class="form-control" name="descripcion" id="descripcion" required>
+        </div>
+        <div class="mb-3">
             <label class="form-label">Estado</label>
             <input type="text" class="form-control" name="estado" id="estado" required>
+        </div>
+        <div class="mb-3">
             <label class="form-label">Fecha de creación</label>
             <input type="datetime-local" class="form-control" name="fechaCreacion" id="fechaCreacion" required>
+        </div>
+        <div class="mb-3">
             <label class="form-label">Fecha de vencimiento</label>
             <input type="datetime-local" class="form-control" name="fechaVencimiento" id="fechaVencimiento" required>
         </div>
-        <button type="submit" class="btn btn-primary">Guardar</button>
-        <a class="btn btn-danger" href="index.php">Cancelar</a>
+        <div class="mb-3">
+            <button type="submit" class="btn btn-success">Guardar</button>
+            <a class="btn btn-danger" href="index.php">Cancelar</a>
+        </div>
     </form>
 </div>
-
 
 <?php
 require_once("C://xampp/htdocs/ProyectoTareas/views/head/footer.php");

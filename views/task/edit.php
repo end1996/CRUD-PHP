96d095ea3766ff0796325b82224898716ed6a5b0
Enmanuel <end199615@gmail.com>
2024-11-14 13:08:54 -0500

    Modificación de estructura del formulario edit.php de row a col,cambio de tamaño de controles y container

--- views/task/edit.php
+++ views/task/edit.php
@@ -6,60 +6,49 @@ $obj = new TaskController();
 //Pruebas de depuración
 $task = $obj->mostrar($_GET['id']);
 ?>
-<div class="text-center text-light mb-5">
-    <h2>Modificando Registro</h2>
-</div>
-<div class="m-5">
+<div class="m-0 w-50 mx-auto">
+    <div class="text-center text-light mb-5">
+        <h2>Modificando Registro</h2>
+    </div>
     <form action="update.php" method="POST" autocomplete="off">
         <!--Contenedor ID-->
-        <div class="mb-3 row">
-            <label for="staticId" class="col-sm-2 col-form-label">ID</label>
-            <div class="col-sm-10">
-                <input type="number" name="id" readonly class="form-control" id="staticId" value="<?= $task[0] ?>">
-            </div>
+        <div class="mb-3 w-50 mx-auto">
+            <label for="staticId" class="form-label">ID</label>
+            <input type="number" name="id" readonly class="form-control" id="staticId" value="<?= $task[0] ?>">
         </div>
         <!--Contenedor Título-->
-        <div class="mb-3 row">
+        <div class="mb-3 w-50 mx-auto">
             <label for="inputTitle" class="col-sm-2 col-form-label">Título</label>
-            <div class="col-sm-10">
-                <input type="text" name="titulo" class="form-control" id="inputTitle" value="<?= $task[1] ?>">
-            </div>
+            <input type="text" name="titulo" class="form-control" id="inputTitle" value="<?= $task[1] ?>">
         </div>
         <!--Contenedor Descripción-->
-        <div class="mb-3 row">
+        <div class="mb-3 w-50 mx-auto">
             <label for="inputDescription" class="col-sm-2 col-form-label">Descripción</label>
-            <div class="col-sm-10">
-                <input type="text" name="descripcion" class="form-control" id="inputDescription"
-                    value="<?= $task[2] ?>">
-            </div>
+            <input type="text" name="descripcion" class="form-control" id="inputDescription" value="<?= $task[2] ?>">
         </div>
         <!--Contenedor Estado-->
-        <div class="mb-3 row">
-            <label for="inputState" class="col-sm-2 col-form-label">Estado</label>
-            <div class="col-sm-10">
-                <!--Operador ternario valida si la palabra contenida coincide con el estado en la base de datos
+        <div class="mb-3 w-50 mx-auto">
+            <label for="inputState" class="form-label">Estado</label>
+            <!--Operador ternario valida si la palabra contenida coincide con el estado en la base de datos
                 de ser asi la define como selected y se muestra esa en el campo, de lo contrario define el campo vacío-->
-                <select name="estado" class="form-control" id="selectEstado" required>
-                    <option value="Backlog" <?= $task[3] == 'Backlog' ? 'selected' : ''; ?>>Backlog</option>
-                    <option value="En curso" <?= $task[3] == 'En curso' ? 'selected' : '' ?>>En curso</option>
-                    <option value="Pendiente" <?= $task[3] == 'Pendiente' ? 'selected' : ''; ?>>Pendiente</option>
-                    <option value="En revisión" <?= $task[3] == 'En revisión' ? 'selected' : ''; ?>>En revisión</option>
-                    <option value="Completada" <?= $task[3] == 'Completada' ? 'selected' : ''; ?>>Completada</option>
-                </select>
-            </div>
+            <select name="estado" class="form-control" id="selectEstado" required>
+                <option value="Backlog" <?= $task[3] == 'Backlog' ? 'selected' : ''; ?>>Backlog</option>
+                <option value="En curso" <?= $task[3] == 'En curso' ? 'selected' : '' ?>>En curso</option>
+                <option value="Pendiente" <?= $task[3] == 'Pendiente' ? 'selected' : ''; ?>>Pendiente</option>
+                <option value="En revisión" <?= $task[3] == 'En revisión' ? 'selected' : ''; ?>>En revisión</option>
+                <option value="Completada" <?= $task[3] == 'Completada' ? 'selected' : ''; ?>>Completada</option>
+            </select>
         </div>
-        <!--Contenedor Fecha de creación-->
-        <div class="mb-3 row">
-            <label for="staticDate" class="col-sm-2 col-form-label">Fecha de creación</label>
-            <div class="col-sm-10">
+        <div class="mb-3 row gx-3 w-50 mx-auto">
+            <!--Contenedor Fecha de creación-->
+            <div class="col">
+                <label for="staticDate" class="form-label">Fecha de creación</label>
                 <input type="datetime-local" name="fechaCreacion" readonly class="form-control" id="staticDate"
                     value="<?= $task[4] ?>">
             </div>
-        </div>
-        <!--Contenedor Fecha de vencimiento-->
-        <div class="mb-3 row">
-            <label for="inputDate" class="col-sm-2 col-form-label">Fecha de vencimiento</label>
-            <div class="col-sm-10">
+            <!--Contenedor Fecha de vencimiento-->
+            <div class="col">
+                <label for="inputDate" class="form-label">Fecha de vencimiento</label>
                 <input type="datetime-local" name="fechaVencimiento" class="form-control" id="inputDate"
                     value="<?= $task[5] ?>" min="<?= fechaHoraActual() ?>">
             </div>
